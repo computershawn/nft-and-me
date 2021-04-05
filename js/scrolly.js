f0787747a06270b23562f139b454d7e85c773143
Shawn J <shontek@gmail.com>
2021-04-04 23:15:49 -0700

    Adjust scroll animations and background gradient

--- js/scrolly.js
+++ js/scrolly.js
@@ -4,24 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const initScrollStuff = () => {
-  // Init ScrollMagic
   const controller = new ScrollMagic.Controller();
 
-  // Enable ScrollMagic only for desktop, disable on touch and mobile devices
   if (!Modernizr.touch) {
-    // SCENE - parallax effect on the intro slide
-    // move bcg container when intro gets out of the the view
-
     const introTl = new TimelineMax();
 
     const introDiv = document.querySelector('#intro');
-    const introBackground = document.querySelector('#intro .bcg');
+    // const introBackground = document.querySelector('#intro .bcg');
     const introText = document.querySelector('#intro-text');
 
     introTl
-      .to(introText, 1.4, { y: '-10%', ease: Power1.easeOut }, '-=0.2')
-      .to(introBackground, 1.4, { y: '20%', ease: Power1.easeOut }, '-=0.2')
-      .to(introDiv, 0.7, { autoAlpha: 0.2, ease: Power1.easeNone }, '-=1.4');
+      .to(introText, 1, { y: '-10%', ease: Power1.easeOut })
+      .to(introDiv, 1, { autoAlpha: 0.2, ease: Power1.easeNone })
 
     const introScene = new ScrollMagic.Scene({
       triggerElement: '#intro',
@@ -31,7 +25,6 @@ const initScrollStuff = () => {
       .setTween(introTl)
       .addTo(controller);
 
-    // change behaviour of controller to animate scroll instead of jump
     controller.scrollTo(function (newpos) {
       TweenMax.to(window, 1, { scrollTo: { y: newpos }, ease: Power1.easeInOut });
     });
